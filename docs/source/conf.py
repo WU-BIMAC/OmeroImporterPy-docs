9117b6a0822471302bb5c1e8538e573a52724291
Caterina Strambio De Castillia <caterina.strambio@umassmed.edu>
2024-05-09 03:28:37 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'OMERO Importer'
+copyright = '2024, Alessandro Rigano, Caterina Strambio De Castillia'
+author = 'Alessandro Rigano, Caterina Strambio De Castillia'
 
 release = '0.1'
 version = '0.1.0'
